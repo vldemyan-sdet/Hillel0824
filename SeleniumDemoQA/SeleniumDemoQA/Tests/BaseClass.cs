94ec5710a4d9d6db0905b8067184f77a3272f09a
Volodymyr Demianenko <vldemyan@outlook.com>
2024-08-27 19:14:57 +0300

    remove extra using

--- SeleniumDemoQA/SeleniumDemoQA/Tests/BaseClass.cs
+++ SeleniumDemoQA/SeleniumDemoQA/Tests/BaseClass.cs
@@ -1,10 +1,5 @@
 ﻿using OpenQA.Selenium.Support.UI;
 using OpenQA.Selenium;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using OpenQA.Selenium.Chrome;
 
 namespace SeleniumDemoQA.Tests
