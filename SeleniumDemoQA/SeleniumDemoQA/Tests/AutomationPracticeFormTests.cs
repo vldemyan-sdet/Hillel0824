ebcc0c664e235def6a63a1c8e5d1330c0a94e791
Volodymyr Demianenko <vldemyan@outlook.com>
2024-08-27 20:35:11 +0300

    commit 2

--- SeleniumDemoQA/SeleniumDemoQA/Tests/AutomationPracticeFormTests.cs
+++ SeleniumDemoQA/SeleniumDemoQA/Tests/AutomationPracticeFormTests.cs
@@ -1,4 +1,5 @@
 ﻿using OpenQA.Selenium;
+using SeleniumDemoQA.Pages;
 
 namespace SeleniumDemoQA.Tests
 {
@@ -9,36 +10,29 @@ namespace SeleniumDemoQA.Tests
         {
             _driver.Navigate().GoToUrl("https://demoqa.com/automation-practice-form");
 
-            FillInput(By.Id("firstName"), "John");
-            FillInput(By.Id("lastName"), "Doe");
+            var formPage = new FormPage(_driver);
+            
 
-            FillInput(By.Id("userEmail"), "johndoe@example.com");
-            ClickElement(By.CssSelector("label[for='gender-radio-1']"));
+            formPage.FillInput(By.Id("firstName"), "John");
+            formPage.FillInput(By.Id("lastName"), "Doe");
+            formPage.FillInput(By.Id("userEmail"), "johndoe@example.com");
+            formPage.ClickElement(By.CssSelector("label[for='gender-radio-1']"));
+            formPage.FillInput(By.Id("userNumber"), "1234567890");
+            formPage.ClickElement(By.Id("dateOfBirthInput"));
+            formPage.SelectByText(By.ClassName("react-datepicker__month-select"), "May");
+            formPage.SelectByText(By.ClassName("react-datepicker__year-select"), "1990");
+            formPage.ClickElement(By.CssSelector(".react-datepicker__day--015:not(.react-datepicker__day--outside-month)"));
+            formPage.FillInput(By.Id("subjectsInput"), "Maths");
+            formPage.GetElementBy(By.Id("subjectsInput")).SendKeys(Keys.Enter);
+            formPage.ClickElement(By.CssSelector("label[for='hobbies-checkbox-1']"));
+            formPage.FillInput(By.Id("currentAddress"), "123 Main Street, Anytown, USA");
+            formPage.ClickElement(By.Id("state"));
+            formPage.ClickElement(By.XPath("//div[text()='NCR']"));
+            formPage.ClickElement(By.Id("city"));
+            formPage.ClickElement(By.XPath("//div[text()='Delhi']"));
+            formPage.ClickElement(By.Id("submit"));
 
-            FillInput(By.Id("userNumber"), "1234567890");
-
-            ClickElement(By.Id("dateOfBirthInput"));
-            SelectByText(By.ClassName("react-datepicker__month-select"), "May");
-            SelectByText(By.ClassName("react-datepicker__year-select"), "1990");
-            ClickElement(By.CssSelector(".react-datepicker__day--015:not(.react-datepicker__day--outside-month)"));
-
-            FillInput(By.Id("subjectsInput"), "Maths");
-            GetElementBy(By.Id("subjectsInput")).SendKeys(Keys.Enter);
-
-            ClickElement(By.CssSelector("label[for='hobbies-checkbox-1']"));
-
-            FillInput(By.Id("currentAddress"), "123 Main Street, Anytown, USA");
-
-            ClickElement(By.Id("state"));
-
-            ClickElement(By.XPath("//div[text()='NCR']"));
-
-            ClickElement(By.Id("city"));
-            ClickElement(By.XPath("//div[text()='Delhi']"));
-
-            ClickElement(By.Id("submit"));
-
-            var confirmationModal = GetElementBy(By.Id("example-modal-sizes-title-lg"));
+            var confirmationModal = formPage.GetElementBy(By.Id("example-modal-sizes-title-lg"));
 
             Assert.IsTrue(confirmationModal.Displayed);
             Assert.That(confirmationModal.Text, Is.EqualTo("Thanks for submitting the form"));
@@ -50,14 +44,16 @@ namespace SeleniumDemoQA.Tests
         {
             _driver.Navigate().GoToUrl("https://demoqa.com/automation-practice-form");
 
-            ClickElement(By.Id("submit"));
+            var formPage = new FormPage(_driver);
+
+            formPage.ClickElement(By.Id("submit"));
             Thread.Sleep(2000);
 
-            string firstNameBorderColor = GetBorderColor(By.Id("firstName"));
+            string firstNameBorderColor = formPage.GetBorderColor(By.Id("firstName"));
 
-            string lastNameBorderColor = GetBorderColor(By.Id("lastName"));
+            string lastNameBorderColor = formPage.GetBorderColor(By.Id("lastName"));
 
-            string mobileNumberBorderColor = GetBorderColor(By.Id("userNumber"));
+            string mobileNumberBorderColor = formPage.GetBorderColor(By.Id("userNumber"));
 
             string expectedBorderColor = "rgb(220, 53, 69)";
 
--- SeleniumDemoQA/SeleniumDemoQA/Tests/TextBoxTests.cs
+++ SeleniumDemoQA/SeleniumDemoQA/Tests/TextBoxTests.cs
@@ -1,6 +1,7 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
+using SeleniumDemoQA.Pages;
 
 namespace SeleniumDemoQA.Tests
 {
@@ -11,24 +12,26 @@ namespace SeleniumDemoQA.Tests
         {
             _driver.Navigate().GoToUrl("https://demoqa.com/text-box");
 
+            var formPage = new FormPage(_driver);
+
             var name = "Sasha B";
             var email = "Sasha@gmail.com";
             var currentAddress = "MyStreet, 100, MyCity, MyCountry, 123456";
             var permanentAddress = "Vesnyana Street, 10, Kyiv, Ukraine, 29004";
 
-            FillInput(By.Id("userName"), name);
-            FillInput(By.Id("userEmail"), email);
-            FillInput(By.Id("currentAddress"), currentAddress);
-            FillInput(By.Id("permanentAddress"), permanentAddress);
+            formPage.FillInput(By.Id("userName"), name);
+            formPage.FillInput(By.Id("userEmail"), email);
+            formPage.FillInput(By.Id("currentAddress"), currentAddress);
+            formPage.FillInput(By.Id("permanentAddress"), permanentAddress);
 
             // Scroll to Submit button and click it
-            ClickElement(By.Id("submit"));
+            formPage.ClickElement(By.Id("submit"));
 
             // Assert that the submitted values are displayed correctly in the output form
-            var nameResult = GetElementBy(By.Id("name"));
-            var emailResult = GetElementBy(By.Id("email"));
-            var currentAddressResult = GetElementBy(By.CssSelector("#output #currentAddress.mb-1"));
-            var permanentAddressResult = GetElementBy(By.CssSelector("#output #permanentAddress.mb-1"));
+            var nameResult = formPage.GetElementBy(By.Id("name"));
+            var emailResult = formPage.GetElementBy(By.Id("email"));
+            var currentAddressResult = formPage.GetElementBy(By.CssSelector("#output #currentAddress.mb-1"));
+            var permanentAddressResult = formPage.GetElementBy(By.CssSelector("#output #permanentAddress.mb-1"));
 
             //The formatting of the output below is not quite right, in particular with the spaces, but leaving as is to pass the test
             Assert.That(nameResult.Text, Is.EqualTo("Name:" + name));
