2ca9e1d6aba1a535a453686ee2d6c8ad757d5446
Volodymyr Demianenko <vldemyan@outlook.com>
2024-10-29 22:25:06 +0200

    Add Evershop.Tests.API

---
+++ Evershop.Tests/Evershop.Tests.API/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Evershop.Tests.API
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
